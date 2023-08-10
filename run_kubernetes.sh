365159f31150b42ccae0f236f32b0ca70bfd261f
EC2 Default User <ec2-user@ip-172-31-8-71.ec2.internal>
2023-08-10 07:00:12 +0000

    project 4: Done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+# dockerpath=
+dockerpath=vhthieu100499/flaskapp_project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run api --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward api 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=vhthieu100499/flaskapp_project4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
